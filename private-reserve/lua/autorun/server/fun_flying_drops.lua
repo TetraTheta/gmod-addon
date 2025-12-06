79d1fb7de1ea8ac79973608f4f7d506304f02632
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2025-12-07 00:25:21 +0900

    fix(private-reserve): Fix Lua error related to Antlion Grub

--- private-reserve/lua/autorun/server/fun_flying_drops.lua
+++ private-reserve/lua/autorun/server/fun_flying_drops.lua
@@ -6,5 +6,6 @@
 --
 hook.Add("OnNPCKilled", "PR_FlyingDrops", function(npc, attacker, _)
   if not GetConVar("pr_enable_flying_drops"):GetBool() then return end
-  if npc:IsValid() and attacker:IsValid() then npc:DropWeapon(nil, attacker:GetPos()) end
+  -- Antlion Grub isn't NPC!
+  if npc:IsValid() and npc:IsNPC() and attacker:IsValid() then npc:DropWeapon(nil, attacker:GetPos()) end
 end)
