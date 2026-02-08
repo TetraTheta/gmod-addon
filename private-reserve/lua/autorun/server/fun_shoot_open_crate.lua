14a1a120cc7836173db4f0dd5d4c1d98f5bc87c0
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-02-08 21:27:54 +0900

    refactor: Type safety

--- private-reserve/lua/autorun/server/fun_shoot_open_crate.lua
+++ private-reserve/lua/autorun/server/fun_shoot_open_crate.lua
@@ -11,7 +11,9 @@ hook.Add("PostEntityFireBullets", "PR_Shoot_Open_AmmoCrate", function(_, fb)
   if not fb.Trace.Hit then return end
   local target = fb.Trace.Entity
   local attacker = fb.Attacker
-  if not (target:IsValid() and attacker:IsValid()) then return end
+  if not (IsValid(target) and IsValid(attacker)) then return end
+  ---@cast target Entity
+  ---@cast attacker Entity
   if attacker:GetClass() ~= "player" or target:GetClass() ~= "item_ammo_crate" then return end
   target:Use(attacker)
 end)
