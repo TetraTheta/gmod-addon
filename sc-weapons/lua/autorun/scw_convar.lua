156a02606d6ad143a6c6f2fa45a27fcb46970bf7
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-02-14 16:03:54 +0900

    feat(sc-weapons): Add MP5 as admin weapon
    I don't have plan to add MP5 as non-admin weapon for now.
    
    I also re-compiled weapon models too.

--- sc-weapons/lua/autorun/scw_convar.lua
+++ sc-weapons/lua/autorun/scw_convar.lua
@@ -4,11 +4,11 @@
 ---@param min number
 ---@param max number
 local function _CreateConVar(convar, description, def, min, max)
-  local flags = {FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_NOTIFY}
+  local flags = { FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXECUTE, FCVAR_REPLICATED, FCVAR_NOTIFY }
   if not ConVarExists(convar) then CreateConVar(convar, def, flags, description, min, max) end
 end
-
 --
+_CreateConVar("scaw_mp5_default", "Default secondary fire mode of SC Admin MP5", "1", 1, 4)
 _CreateConVar("scaw_mp5sd_default", "Default secondary fire mode of SC Admin MP5SD", "1", 1, 4)
 _CreateConVar("scaw_owner_immune_explosion", "Mark owner of the SC Admin Weapon immune to the explosion from Explosion Mode", "0", 0, 1)
 _CreateConVar("scaw_pistol_default", "Default secondary fire mode of SC Admin Pistol", "1", 1, 4)
