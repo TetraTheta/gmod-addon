3c53660d4d1e5743280b97cf494f89c8427b54e4
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-04-25 16:16:22 +0900

    fix(private-reserve): NPC가 무기 미소유 시 오류 처리

--- private-reserve/lua/autorun/server/fun_flying_drops.lua
+++ private-reserve/lua/autorun/server/fun_flying_drops.lua
@@ -10,5 +10,10 @@ hook.Add("OnNPCKilled", "PR_FlyingDrops", function(npc, attacker, _)
   if not cv then cv = GetConVar("pr_enable_flying_drops") end
   if not cv:GetBool() then return end
   -- Antlion Grub isn't NPC!
-  if npc:IsValid() and npc:IsNPC() and attacker:IsValid() then npc:DropWeapon(nil, attacker:GetPos()) end
+  if npc:IsValid() and npc:IsNPC() and attacker:IsValid() then
+    -- Some custom NPC does not carry weapon
+    if npc:GetActiveWeapon():IsValid() then
+      npc:DropWeapon(nil, attacker:GetPos())
+    end
+  end
 end)
