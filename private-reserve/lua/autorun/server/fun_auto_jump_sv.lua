9bd2b860cb379f683185f0638d877f4b60d47f84
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-06-15 22:23:44 +0900

    260615

--- private-reserve/lua/autorun/server/fun_auto_jump_sv.lua
+++ private-reserve/lua/autorun/server/fun_auto_jump_sv.lua
@@ -32,7 +32,7 @@ local function set_server_spam_state(ply, is_active)
 
   local player_name = ply:Nick()
   local state_text = is_active and "enabled" or "disabled"
-  ServerLog(string.format("[AutoJump] Server-managed jump spam %s for %s (%s)\n", state_text, player_name, steam_id))
+  --ServerLog(string.format("[AutoJump] Server-managed jump spam %s for %s (%s)\n", state_text, player_name, steam_id))
 end
 
 hook.Add("StartCommand", "PR_AutoJump_ServerSpam", function(ply, cmd)
