088dba98feff285e29632b2b06131015c5a2f399
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-07-07 20:56:52 +0900

    feat(private-reserve): 최대 체력/방어구 수치 조절

---
+++ private-reserve/lua/autorun/server/fun_max_armor.lua
@@ -0,0 +1,16 @@
+local cv = GetConVar("sk_suitcharger_citadel_maxarmor")
+--[[
+################
+#     HOOK     #
+################
+]]
+hook.Add("PlayerSpawn", "PR_PlayerMaxArmor", function(ply, is_transition)
+  local max_health = 200
+  local max_armor = cv:GetInt() > 100 and cv:GetInt() or 500
+  timer.Simple(0, function()
+    ply:SetMaxHealth(max_health)
+    if not is_transition then ply:SetHealth(max_health) end
+    ply:SetMaxArmor(max_armor)
+  end)
+  print("maxarmor: " .. max_armor)
+end)
