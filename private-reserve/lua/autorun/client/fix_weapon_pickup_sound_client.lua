30dfaf9c3a9c0d479ea7fca1831beb39bceb46e2
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-07-14 22:02:16 +0900

    feat(private-reserve): 크로우바/중력건 픽업 알림 및 사운드

---
+++ private-reserve/lua/autorun/client/fix_weapon_pickup_sound_client.lua
@@ -0,0 +1,13 @@
+net.Receive("PR_FixWeaponPickup_Notification", function()
+  local class = net.ReadString()
+  timer.Simple(0, function()
+    local ply = LocalPlayer()
+    if not IsValid(ply) or not ply:Alive() then return end
+
+    local wep = ply:GetWeapon(class)
+    if not IsValid(wep) then return end
+
+    GAMEMODE:HUDWeaponPickedUp(wep)
+    surface.PlaySound("items/ammo_pickup.wav")
+  end)
+end)
