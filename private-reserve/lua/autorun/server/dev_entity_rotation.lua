8cea86fc485184210516136adfbdde4a33d791ef
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-07-27 22:56:02 +0900

    feat(private-reserve): 'decode_dupe', 'encode_dupe', 'set_entity_angle' 추가
    'set_entity_angle'은 NPC 상대로는 잘 작동하지 않음.

---
+++ private-reserve/lua/autorun/server/dev_entity_rotation.lua
@@ -0,0 +1,24 @@
+concommand.Add("set_entity_angle", function(ply, _, args)
+  if not IsValid(ply) then return end
+  local tr = ply:GetEyeTrace()
+  local ent = tr.Entity
+  if IsValid(ent) then
+    ---@cast ent Entity
+    local pitch = tonumber(args[1]) or 0
+    local yaw = tonumber(args[2]) or 0
+    local roll = tonumber(args[3]) or 0
+    local targetAng = Angle(pitch, yaw, roll)
+    ent:SetAngles(targetAng)
+    local phy = ent:GetPhysicsObject()
+    if IsValid(phy) then
+      phy:Wake()
+      phy:SetAngles(targetAng)
+    end
+    if ent:IsNPC() then
+      ent:SetIdealYawAndUpdate(yaw)
+    end
+    ply:ChatPrint("Entity Angles changed: " .. tostring(targetAng))
+  else
+    ply:ChatPrint("No valid entity or model")
+  end
+end)
