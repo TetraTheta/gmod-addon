112f1885f02a4d588729592454f2a5bcc9a52b3d
TetraTheta <37061801+TetraTheta@users.noreply.github.com>
2026-08-27 01:31:07 +0900

    feat(fix-map): Precursor 헌터 조기 사망시 softlock 방지

--- fix-map/lua/autorun/server/fixmap_precursor.lua
+++ fix-map/lua/autorun/server/fixmap_precursor.lua
@@ -3,9 +3,13 @@ Mod: Precursor
 Map:
 - r_map7
 ]]
-hook.Add("InitPostEntity", "FixMap_Precursor_InitPostEntity", function()
-  if SERVER and game.GetMap() == "r_map7" then
-    local tm = ents.FindByName("hunter_boss")[1]
-    tm:Input("AddOutput", tm, nil, "OnDeath hunter_retreat_relay,Trigger,,0,1")
-  end
+
+---@param ent Entity
+hook.Add("OnEntityCreated", "FixMap_Precursor_OnEntityCreated", function(ent)
+  if game.GetMap() ~= "r_map7" then return end
+  timer.Simple(0, function()
+    if not IsValid(ent) or ent:GetClass() ~= "npc_hunter" or ent:GetName() ~= "hunter_boss" then return end
+    ent:Input("AddOutput", ent, ent, "OnDeath hunter_retreat_relay,Trigger,,0,1")
+    ent:Input("AddOutput", ent, ent, "OnDeath fence_break,Break,,0,1")
+  end)
 end)
